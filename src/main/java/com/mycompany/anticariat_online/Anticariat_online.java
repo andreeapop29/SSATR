02379926a2b02577e9884bd238ba8e1a748981a3
andre <andre@DESKTOP-DRQVPCP>
2024-02-23 15:57:37 +0200

    First commit!

---
+++ src/main/java/com/mycompany/anticariat_online/Anticariat_online.java
@@ -0,0 +1,16 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package com.mycompany.anticariat_online;
+
+/**
+ *
+ * @author andre
+ */
+public class Anticariat_online {
+
+    public static void main(String[] args) {
+       
+    }
+}
